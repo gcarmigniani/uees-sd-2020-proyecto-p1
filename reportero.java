461e127b128147cefbf7b4a9486a1999544608c7
unknown <gcarmigniani@uees.edu.ec>
2020-10-20 21:47:03 -0500

    proyecto primer parcial 1.1

--- reportero.java
+++ reportero.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 
 import java.io.*;
 import java.net.*;
-import java.util.Iterator;
 
 // Esta clase utilizara el Servidor que recibira los numeros
 public class reportero {
@@ -22,24 +21,33 @@ public class reportero {
 
     public static void main(String[] args) {
 
+        // Se llama a una funcion que esperara que el Verificador envio datos de los
+        // sensores
         awaitSensorData();
     }
 
+    // Esta funcion crea un Socket que espera al los datos enviados por el
+    // Verificador
     public static void awaitSensorData() {
 
         try {
-            // Se crea un ServerSocket para esperar los mensajes
+
+            // Se crea un ServerSocket para esperar los datos
             ServerSocket ss = new ServerSocket(puerto);
-            System.out.println("Esperando a recibir mensaje en el puerto: 9999\n");
+            System.out.println("Esperando a recibir datos en el puerto: 9999\n");
             Socket s = ss.accept();
 
-            // Se crea un data inputStream que espera el numero de parte del cliente
+            // Se crea un data inputStream que espera los datos
             DataInputStream dis = new DataInputStream(s.getInputStream());
 
+            // Los datos son de tipo JSON en formato String UTF, por lo que se debe hacer el
+            // unmarshalling
             String encodedJSON = (String) dis.readUTF();
-           
+
             System.out.println(encodedJSON);
             System.out.println("Mensaje JSON recibido, procediendo al unmarshalling\n");
+
+            // Se envia los datos recibidos para hacer el unmarshalling
 
             readSensorData(encodedJSON);
             // Se cierra el puerto del servidor
@@ -57,29 +65,37 @@ public class reportero {
 
     }
 
+    // Este funcion recibe el JSON en formato String, y lo parsea en un JSONObject
     public static void readSensorData(String encodedJSON) {
 
+        // Se crea un parser para crear el objecto JSONObject
         JSONParser parser = new JSONParser();
+
         String agentId;
         JSONObject sensorData;
         String time;
 
         try {
-
+            // sensorData el es Objeto JSON que tendra la informacion de nuestro sensor
             sensorData = (JSONObject) parser.parse(encodedJSON);
             agentId = sensorData.get("agente").toString();
             time = sensorData.get("fechahoraUTC").toString();
-
+            // Las lecturas del sensor seran manejadas en un JSONArray
             JSONArray lecturas = (JSONArray) sensorData.get("lecturas");
 
             String nombreSensor;
             double lecturaValor;
-           
 
+            // Para actualizar cada registro de sensor de cada agente, se crea un loop que
+            // recorrera todos los sensores de una lectura con su respectivo agente y
+            // buscara si tal registro exista para lo cual llamara la funcion
+            // updateSensorLog enviados los datos necesarios para crear o actualizar los
+            // registros de casa sensor
             for (int i = 0; i < lecturas.size(); i++) {
+
                 JSONObject itemObj = (JSONObject) lecturas.get(i);
                 nombreSensor = (String) itemObj.get("sensor");
-                lecturaValor =((Number) itemObj.get("lectura")).doubleValue();
+                lecturaValor = ((Number) itemObj.get("lectura")).doubleValue();
 
                 updateSensorLog(agentId, nombreSensor, lecturaValor, time);
             }
@@ -90,34 +106,45 @@ public class reportero {
 
     }
 
+    // Esta funcion lee los regitros existentes, o los crea si no existen
+    // Tambien actualiza los registros con las nuevas lectura, y calcula el max, min
+    // y med de las lecturas
     public static void updateSensorLog(String agentId, String sensorId, double sensorValue, String time) {
 
         JSONObject sensorLog;
 
+        // Se asigna el nombre de archivo con el nombre del agente y el nombre del
+        // sensor
         String filename = agentId + "-" + sensorId + ".json";
 
+        // Se busca si el archivo de registro existe en la carpeta de registros
         File f = new File("./registros/" + filename);
         if (f.exists() && !f.isDirectory()) {
 
             System.out.println("Se econtro el registro del sensor: " + sensorId + " del agente: " + agentId);
             System.out.println("Ingresando el nuevo valor de lectura: " + sensorValue);
 
+            // Si el registro existe, se lo lee y se lo asigna a sensorLog
             sensorLog = readJSON("./registros/" + agentId + "-" + sensorId + ".json");
 
+            // Para saber si la nueva lectura cambia el minimo o maximo del registro,
+            // se lee el Maximo y el Minimo, y se lo compare con el valor de la lectura del
+            // sensor
             double logMin = (double) sensorLog.get("lectura_min");
             double logMax = (double) sensorLog.get("lectura_max");
 
             if (sensorValue < logMin) {
+                // si el valor es menor, se actualiza el minimo
                 logMin = sensorValue;
             }
             if (sensorValue > logMax) {
+                // si el valor es mayor, se actualiza el maximo
                 logMax = sensorValue;
             }
 
-           
-
-            
-
+            // Para encontrar la nueva media de las lecturas, es necesario tomar las
+            // lecturas como un JSONArray, a la cual se le agrega un nuevo JSONObject con la
+            // nueva lectura
             JSONArray sensorLogArray = (JSONArray) sensorLog.get("lecturas");
 
             JSONObject lectureObj = new JSONObject();
@@ -125,36 +152,50 @@ public class reportero {
             lectureObj.put("lectura", sensorValue);
 
             sensorLogArray.add(lectureObj);
-            
+
+            // Una vez agregado la nueva lectura, se itera en el array, sumando el total de
+            // todos los valores de la lectura, y dividiendola por el numero total de
+            // lecturas
+            // para obtener el valor de la media de las lecturas
 
             double logMed = 0;
 
             for (int i = 0; i < sensorLogArray.size(); i++) {
-                System.out.println(((JSONObject)sensorLogArray.get(i)).get("lectura"));
-                logMed +=((Number) ((JSONObject)sensorLogArray.get(i)).get("lectura")).doubleValue();
+                System.out.println(((JSONObject) sensorLogArray.get(i)).get("lectura"));
+                logMed += ((Number) ((JSONObject) sensorLogArray.get(i)).get("lectura")).doubleValue();
             }
 
             logMed = logMed / sensorLogArray.size();
 
+            // Finalmente se agregan los valores actualizaron al JSON
+
             sensorLog.put("lectura_max", logMax);
             sensorLog.put("lectura_min", logMin);
             sensorLog.put("lectura_med", logMed);
-
             sensorLog.put("lecturas", sensorLogArray);
 
         } else {
 
+            // En caso de que el archivo de registro no existe, se creara el archivo
+            // pertinente
+
             System.out.println("No existe el registro del sensor: " + sensorId + " del agente: " + agentId);
 
-            // create file
+            // Primero se crea el archivo JSON, y se agregan los datos pertinentes al objeto
 
             sensorLog = new JSONObject();
 
             sensorLog.put("agente", agentId);
             sensorLog.put("sensor", sensorId);
+
+            // Debido a que solo hay un valor de lectura al crear el archivo, este equivale
+            // al minimo, maximo y media por lo que no hay que hacer otros calculos
+
             sensorLog.put("lectura_min", sensorValue);
             sensorLog.put("lectura_max", sensorValue);
             sensorLog.put("lectura_med", sensorValue);
+
+            // Se crea un JSONArray para las lecturas y se agrega la lectura enviada
 
             JSONArray sensorLogArray = new JSONArray();
 
@@ -165,22 +206,25 @@ public class reportero {
             sensorLogArray.add(lectureObj);
             sensorLog.put("lecturas", sensorLogArray);
 
-    
         }
+
+        // Una vez creado o modificado el archivo de registro, se utiliza Filewriter
+        // para guardarlo en el disco, utilizando el filename que equivale al nombre del
+        // agente y el nombre del sensor pertintente
 
         try (FileWriter file = new FileWriter("registros/" + filename)) {
 
             file.write(sensorLog.toJSONString());
             file.flush();
 
+            // Cuando se guarda el archivo, se muestra por consola una confirmacion, ademas
+            // de los datos pertinentes como el agente, el sensor y los valores del maximo,
+            // minimo y media actualizados
+
             System.out.println("--- Se ha actualizado el registro exitosamente");
-            System.out.println(
-                "Cambios: Agente: " + agentId + 
-                ", Sensor: " + sensorId + 
-                ", Minima: "+sensorLog.get("lectura_min")+
-                ", Maxima: "+sensorLog.get("lectura_max")+
-                ", Media: "+sensorLog.get("lectura_med")
-                );
+            System.out.println("Cambios: Agente: " + agentId + ", Sensor: " + sensorId + ", Minima: "
+                    + sensorLog.get("lectura_min") + ", Maxima: " + sensorLog.get("lectura_max") + ", Media: "
+                    + sensorLog.get("lectura_med"));
 
         } catch (IOException e) {
 
@@ -190,17 +234,20 @@ public class reportero {
 
     }
 
+    // Esta funcion recibe la ubicacion del JSON y retorna un JSONObject
     public static JSONObject readJSON(String url) {
-        // JSON parser object to parse read file
+
+        // JSON parser para crear el JSON a partir del archivo leido
         JSONParser jsonParser = new JSONParser();
 
         try (FileReader reader = new FileReader(url)) {
-            // Read JSON file
+            // Se lee el archivo JSON
             Object obj = jsonParser.parse(reader);
 
-            // JSONArray JSONsensor = (JSONArray) obj;
+            // se crea un JSONObject con el resultado del parser
             JSONObject response = (JSONObject) obj;
-            // System.out.println(response);
+
+            // Se retorna el JSONObject
             return response;
 
         } catch (FileNotFoundException e) {
--- verificador.java
+++ verificador.java
@@ -10,8 +10,6 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -19,7 +17,6 @@ import java.nio.file.*;
 import java.net.*;
 import java.util.concurrent.TimeUnit;
 
-// Esta clase utilizara el cliente para enviar los numeros al servidor
 public class verificador {
 
     // este puerto se utilizara para la comunicacion
@@ -27,27 +24,38 @@ public class verificador {
 
     public static void main(String[] args) {
 
+        // El Verificador arranca con una funcion que verifica la carpeta donde se
+        // guardan todos los archivos de lectura de sensor subidas por los agentes
         checkSensorDataFolder();
 
     }
 
+    // Esta funcion periodicamente busca en la carpeta asignada por nuevos archivos
+    // de tipo JSON. En caso de encontrar un JSON con el formato de lectura de
+    // sensor, primero busca si el archivo ha sido previamente leido en
+    // history.json, si ya sido leido anteriormente, lo mueve a la carpeta borrados,
+    // caso contrario lo procesa para enviarlo al Reportero. Ademas en caso de que
+    // encuentre un archivo de no sea de extension .JSON, lo movera a la carpeta
+    // "otros"
     public static void checkSensorDataFolder() {
-
-        JSONObject history = readJSON("./lecturas/history.json");
 
         File folder = new File("./lecturas");
         File[] listOfFiles = folder.listFiles();
 
         System.out.println("\n Archivos en la carpeta lectura: \n");
 
+        // Se crea un array de todos los archivos en la carpeta y se itera cada uno
         for (int i = 0; i < listOfFiles.length; i++) {
 
             if (listOfFiles[i].isFile()) {
 
                 System.out.println("\n- " + listOfFiles[i].getName());
 
+                // Se extrae la extension del nombre del archivo
                 String extension = listOfFiles[i].getName().substring(listOfFiles[i].getName().length() - 5);
 
+                // Si la extension no es de formato .JSON, se mueve al archivo a la carpeta
+                // "otros"
                 if (!extension.equalsIgnoreCase(".json")) {
                     System.out.println(
                             "--- El archivo " + listOfFiles[i].getName() + " no es JSON, moviendolo a carpeta 'otros'");
@@ -61,11 +69,16 @@ public class verificador {
 
                         e.printStackTrace();
                     }
-
+                    // Si el archivo si es de formato .JSON, se busca que comienze con "ag" que
+                    // significa que lo envio un agente, por ejemplo
+                    // "ag002_2020-10-02T20-12-34.json"
                 } else if (listOfFiles[i].getName().substring(0, 2).equalsIgnoreCase("ag")) {
 
                     System.out.println("--- El archivo es un reporte de sensor, Buscando en historial...");
 
+                    // Aqui se llama esta funcion para confirmar si el archivo se ha leido
+                    // previamente, en caso de haberlo leido antes, se lo movera a la carpeta
+                    // "borrados", caso contrario continuara con el envio
                     if (checkIfSensorDataExists(listOfFiles[i].getName())) {
 
                         System.out.println("----- El archivo ya ha sido previamente leido, se lo borrara.");
@@ -84,6 +97,8 @@ public class verificador {
 
                     } else {
 
+                        // En caso de ser un nuevo archivo de lectura, se llamara a la funcion que lo
+                        // envia al reportero
                         System.out.println("----- El archivo es nuevo, intentando enviar al Reportero...");
                         sendSensorData(listOfFiles[i].getName());
 
@@ -91,16 +106,22 @@ public class verificador {
 
                 } else {
 
+                    // Si el archivo es .JSON pero no es del formato de lectura, se lo ignora
                     System.out.println("--- El archivo es JSON pero no es un reporte de sensor");
                 }
 
             } else if (listOfFiles[i].isDirectory()) {
-                // System.out.println("Directory " + listOfFiles[i].getName());
+                // En caso de encontrar un directorio, no hace nada
             }
         }
 
         System.out.println("\n----------------------");
         System.out.println("\n Se ha procesado la carpeta de lecturas correctamente, buscando cambios en 10 segundos");
+
+        // Una vez terminado de examinar la carpeta, la funcion espera una cantidad de
+        // segundos antes de volver a llamarse asi misma recursivamente, lo que permite
+        // que el programa este constantemente procesando la carpeta para encontrar
+        // nuevos archivos de lectura subidos por los agentes
         try {
             TimeUnit.SECONDS.sleep(10);
             checkSensorDataFolder();
@@ -108,23 +129,36 @@ public class verificador {
             System.out.println(ex);
         }
 
-        // Checks sensor data
-
     }
 
+    // Este funcion busca si el nombre del archivo de lectura enviado ya existe en
+    // el historial, si lo encuentra devuelve verdadero, si no lo encuentra devuelve
+    // falso
     public static boolean checkIfSensorDataExists(String filename) {
 
         JSONObject sensorDataHistoryJSON = readJSON("./lecturas/history.json");
-
         JSONArray sensorDataHistoryArrayJSON = (JSONArray) sensorDataHistoryJSON.get("historial");
 
+        // Se crea un ArrayJSON de las lecturas previas, y este array se lo transforma
+        // en un String, dentro de este String generado se utiliza la funcion .contains
+        // para buscar si el nombre del archivo se encuentra en el historial de lecturas
+        // y se devuelve el boolean del resultado
         return sensorDataHistoryArrayJSON.toString().contains(filename);
 
     }
 
+    // Cuando se confirma que el archivo es de formato JSON y es una lectura de
+    // sensor, se lo abre llamando a la funcion readJSON que devuelve un objeto JSON
+    // del archivo, con este archivo se habre un puerto defino y se crea un
+    // DataStream para enviar al Reportero que opera en otro sistema
     public static boolean sendSensorData(String filename) {
 
         JSONObject sensorDataJSON = readJSON("./lecturas/" + filename);
+
+        // Se transforma el objeto JSON en un String para poder enviarlo en formato UTF
+        // por el DataStream, lo que permite tambien la operabilidad con sistemas que no
+        // necesarimente utilzien Java pero pueden hacer uso del JSON en formato String
+
         String encodedJSON = sensorDataJSON.toString();
 
         try {
@@ -135,8 +169,10 @@ public class verificador {
             // Se crea un Stream que enviara la informacion al servidor
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
+            // Se escribe el String JSON en formato UTF
             dout.writeUTF(encodedJSON);
 
+            // Se envia el mensaje
             dout.flush();
 
             // Se cierra el Stream
@@ -147,7 +183,14 @@ public class verificador {
 
             System.out.println("----- El archivo fue enviado al reportero correctamente.");
 
+            // Una vez enviado el archivo, se agrega el nombre del archivo al historial para
+            // no volver a enviar un archivo duplicado en el futuro
             addSentDataToHistory(filename);
+
+            // Ye enviado el archivo, se llama de nuevoi a la funcion que revisa la carpeta
+            // de lecturas, pero se espera 1 segundo antes de volver a llamarla para de esa
+            // forma evitar el congestionamiento del DataStream o del Reportero, dandole
+            // tiempo para procesar cada lectura enviada
             try {
                 TimeUnit.SECONDS.sleep(1);
                 checkSensorDataFolder();
@@ -157,30 +200,42 @@ public class verificador {
 
         } catch (Exception e) {
 
+            // En caso de no poder enviar el mensaje con el JSON al reportero, se muestra el
+            // mensaje e intenta repetir el envio del mensaje al reportero cada 3 segundos
+            // hasta que responda
             System.out.println(e);
             System.out.println("No se pudo comunicar con el reportero, intentando nuevamente en 3 segundos...");
+            
             try {
                 TimeUnit.SECONDS.sleep(3);
             } catch (Exception ex) {
                 System.out.println(ex);
             }
-
+            // Se llama a la funcion recursivamente si no se efectuo el envio del mensaje
             sendSensorData(filename);
             return false;
 
         }
+        // En caso de enviarse el mensaje correctamente, termina la funcion y regresa
+        // verdadero
         return true;
 
     }
 
+    // Esta funcion toma el nombre del archivo enviado y lo agrega la lista de
+    // historial par que no enviado duplicadamente
     public static void addSentDataToHistory(String filename) {
 
+        // Se carga el JSON history.json que contiene los nombres de los archivos
+        // enviados
         JSONObject sensorDataHistoryJSON = readJSON("./lecturas/history.json");
         JSONArray sensorDataHistoryArrayJSON = (JSONArray) sensorDataHistoryJSON.get("historial");
 
+        // Se agrega el nombre del archivo de la lectura enviada al historial
         sensorDataHistoryArrayJSON.add(filename);
         sensorDataHistoryJSON.put("historial", sensorDataHistoryArrayJSON);
 
+        // Se guarda el archivo JSON historial dentro de la carpeta lecturas
         try (FileWriter file = new FileWriter("lecturas/history.json")) {
 
             file.write(sensorDataHistoryJSON.toJSONString());
@@ -196,25 +251,9 @@ public class verificador {
 
     }
 
-
-    public static void parseSensorData(JSONObject sensorDataJSON) {
-
-        String agente = (String) sensorDataJSON.get("agente");
-        String fechahoraUTC = (String) sensorDataJSON.get("fechahoraUTC");
-        System.out.println(agente);
-        System.out.println(fechahoraUTC);
-
-        JSONArray lecturasArray = (JSONArray) sensorDataJSON.get("lecturas");
-
-        for (int i = 0; i < lecturasArray.size(); i++) {
-            System.out.println(lecturasArray.get(i));
-        }
-    }
-
-
     // Esta funcion recibe la ubicacion del JSON y retorna un JSONObject
     public static JSONObject readJSON(String url) {
-        
+
         // JSON parser para crear el JSON a partir del archivo leido
         JSONParser jsonParser = new JSONParser();
 
@@ -222,12 +261,10 @@ public class verificador {
             // Se lee el archivo JSON
             Object obj = jsonParser.parse(reader);
 
-           
             // se crea un JSONObject con el resultado del parser
             JSONObject response = (JSONObject) obj;
-           
 
-            // Se retorna el JSONObject 
+            // Se retorna el JSONObject
             return response;
 
         } catch (FileNotFoundException e) {
